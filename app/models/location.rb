d270bc79435a833e85b983b17755c218ad8d6417
Timo H <timo@dealmachine.net>
2012-06-25 21:14:42 +0300

    Save venue input in hiddens

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,6 +3,8 @@ class Location
   include Mongoid::Timestamps
   field :name, :type => String
   field :foursq_id, :type => String
+  field :description, :type => String
+  field :url_to_fsq, :type => String
   field :street_address, :type => String
   field :cross_street, :type => String
   field :city, :type => String
