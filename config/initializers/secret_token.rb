d5944ff23104fca2ade1dddbb4d2b0c2bbf4a30b
Timo H <timo@dealmachine.net>
2012-06-25 14:49:30 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seshly::Application.config.secret_token = 'a33700b919800cb3dc3536f54fd90ae7cb851df2b2c497df917431fb725b228a48daa578e6134d45370d18faec7cee5319ba9bc30505158b5275fdb4b48188db'
