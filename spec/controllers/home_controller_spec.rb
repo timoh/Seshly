581795f891de3ef120e5fb519a258f227fd1d11b
Timo H <timo@dealmachine.net>
2012-06-26 00:05:37 +0300

    Minor changes

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -10,8 +10,14 @@ describe HomeController do
   end
   
   describe "content" do
-    it "should include posts"
-    
+    it "should include posts" do
+      visit root_path
+      page.should have_content("Posts")
+    end
+  end
+  
+  describe "when a user is not logged in" do
+    it "should encourage to sign up by redirecting to signup when user tries to create a new post"
   end
 
 end
