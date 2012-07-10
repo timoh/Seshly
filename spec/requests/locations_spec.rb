b9b1c212c12fb508c1d190fc3e2b6d3db158d957
Timo H <timo@dealmachine.net>
2012-07-10 03:30:13 +0300

    Correct file for post request specs

--- spec/requests/locations_spec.rb
+++ spec/requests/locations_spec.rb
@@ -1,33 +1,3 @@
 require 'spec_helper'
 
-describe "Locations" do
-  
-  before do 
-    user = FactoryGirl.create(:user)
-    @post = FactoryGirl.create(:post, :user => user)
-  end
-  
-  context '#show' do
-    
-    describe 'when not signed in' do
-      #example: coming from Twitter message
-      it 'should only show basic information' do
-        visit '/posts/'+@post.id.to_s
-        page.should have_content('on')
-      end
-    end
-  
-    describe 'when signed in' do
-  
-      before do
-        visit '/auth/twitter'
-      end
-  
-      it 'should only show basic information' do
-        visit '/posts/'+@post.id.to_s
-        page.should have_content('Edit')
-      end
-    end
-  end
-    
-end
+describe "Locations"
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,3 +1,52 @@
 require 'spec_helper'
 
-describe "Posts" 
+describe "Posts" do
+  
+  before do 
+    user = FactoryGirl.create(:user, :nickname => "Bobbiezzz")
+    @post = FactoryGirl.create(:post, :user => user, :description => "Morjesta")
+  end
+  
+  context '#show' do
+    
+    describe 'when not signed in' do
+      #example: coming from Twitter message
+      it 'should only show basic information' do
+        visit '/posts/'+@post.id.to_s
+        page.should have_content('Bobbiezzz')
+        page.should have_content('Morjesta')
+        page.should_not have_content('See more')
+        page.should_not have_content('Edit')
+      end
+      
+      it 'should not let the user to view index' do
+        visit '/posts/'
+        page.should have_content('You need to sign in')
+      end
+      
+      it 'should not let the user to edit' do
+        visit '/posts/'+@post.id.to_s+'/edit' 
+        page.should have_content('You need to sign in')
+      end
+      
+      it 'should not let the user to post' do
+        visit '/posts/new'
+        page.should have_content('You need to sign in')
+      end
+      
+    end
+  
+    describe 'when signed in' do
+  
+      before do
+        visit '/auth/twitter'
+      end
+  
+      it 'should only show basic information' do
+        visit '/posts/'+@post.id.to_s
+        page.should have_content('Edit')
+      end
+    end
+  end
+    
+end
