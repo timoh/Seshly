f000a4d73e3bb0e8cbaf541fe94e7bbd2e760626
Timo H <timo@dealmachine.net>
2012-07-10 03:26:58 +0300

    Show posts also to not signed users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:show]
   # GET /posts
   # GET /posts.json
   def index
