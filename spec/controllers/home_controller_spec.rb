36fedc6989e1e4418c750b4512876de3f4b99a2e
Timo H <timo@dealmachine.net>
2012-07-11 17:24:02 +0300

    Removed duplicate spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -19,10 +19,5 @@ describe HomeController do
       page.should have_content("Posts")
     end
   end
-  
-  describe "when a user is not logged in" do
-    it "should encourage to sign up by redirecting to signup when user tries to create a new post"
-    
-  end
 
 end
