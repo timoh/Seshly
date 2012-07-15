a0725f1002c5910a01b87ff9932ada20a9f345c0
Timo H <timo@dealmachine.net>
2012-07-16 02:20:42 +0300

    Setup ENV variables for SMTP server for HEROKU

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,11 +2,11 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "efekta.fi",
-  :user_name            => "timo.herttua",
-  :password             => "puhas4XEp3Er5H()",
+  :user_name            => ENV['GMAIL_USERNAME'],
+  :password             => ENV['GMAIL_PASSWORD'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.default_url_options[:host] = "sesh-ly.herokuapp.com"
 Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
