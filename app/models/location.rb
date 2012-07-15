25ac7e7165295f95788e92791433b817d0cfa4d7
Timo H <timo@dealmachine.net>
2012-07-15 17:54:17 +0300

    trying to use geocoder, failing..

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,11 @@
 class Location
   include Mongoid::Document
   include Mongoid::Timestamps
+  # include Geocoder::Model::Mongoid
+  # geocoded_by :address, :latitude => :lat, :longitude => :lng, :skip_index => true # can also be an IP address
+  #after_validation :geocode          # auto-fetch coordinates
+  
+  
   field :name, :type => String
   field :foursq_id, :type => String
   field :description, :type => String
@@ -13,6 +18,22 @@ class Location
   field :country, :type => String
   field :lat, :type => Float
   field :lng, :type => Float
+  
+  # for geocoding, return an array of coordinates
+  def coordinates
+    coordinates = Array.new
+    
+    coordinates.push self.lat
+    coordinates.push self.lng
+    
+    return coordinates
+  end
+  
+  # for reverse geocoding, return address
+  def address
+    address = "#{self.street_address}, #{self.city}, #{self.country}"
+    
+  end
   
   has_many :posts
 
